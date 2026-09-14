f0742b22dc2ba2edc2174e167f2f4d76492a3329
bioeggs-hd <bagars192@gmail.com>
2026-09-14 10:37:55 +0200

    Sdifun
    Isidnc

--- Kckeifkv.lua
+++ Kckeifkv.lua
@@ -10,8 +10,21 @@ const OFFSETS = {
     {0.5, 0},
     {-0.5, 0}
   },
-  [
+  [3] = {
+    {0, 0.433},
+    {0.5, -0.433},
+    {-0.5, -0.433}
+  },
+  -- ...
 }
+
+--// FUNCTIONS
+
+local function _GetVector(vector2: {number, number}): Vector3
+  return Vector3.new(vector2[1], 0, vector2[2])
+end
+
+--// MODULE
 
 local MathModule = {}
 
